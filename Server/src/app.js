b766046f07f78e9b87cdd28e95414c5731c4e9a4
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2025-04-14 14:30:49 +0530

    feat(Ai Api EndPoint): create a ai api end point

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-
+import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
     origin: `${process.env.CORS_ORIGIN}`,
-
 }))
 
 app.use(express.json({ limit: '20kb' }));
+app.use(express.urlencoded({ extended: true, limit: '20kb' }));
+app.use(cookieParser());
+app.use(express.static('public'));
 
+
+import aiApiRoutes from './routes/generate.routes.js';
+
+app.use('/api/v1/generate', aiApiRoutes);
 
 export default app; 
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,15 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
-
 import app from './app.js';
 
-const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('Welcome! Your server is working.');
 });
 
 
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log("Server is running on port : http://localhost:" + PORT);
 })
